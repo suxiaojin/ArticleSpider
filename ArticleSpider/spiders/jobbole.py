b1b21ef7bd02671614276468c4b34de8dc4d06bc
suxiaojin <suxiaojin928380@gmail.com>
2017-08-07 17:59:25 +0800

    jobbole

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/112085/']
 
     def parse(self, response):
+        title=response.xpath('//*[@id="post-112085"]/div[1]/h1/text()')
+        create_date=response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip()
+        praise_numbers=int(response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0])
+        fav_numbers = int(response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0])
+        match_re=re.match(".*(\d+).*",fav_numbers)
+        if match_re:
+            fav_numbers=match_re.group(1)
+        comment_nums = response.xpath('//a[@href="#article-comment"]/span').extract()
+        match_re = re.match(".*(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+        content=response.xpath("//div[@class='entry']").extract()
         pass
